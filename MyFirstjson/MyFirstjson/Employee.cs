b660773a17ec4f7d686ad71b2001c297f99a2faf
Lohitha Yerramsetty <lohitha.yerramsetty@gmail.com>
2020-09-07 11:41:15 +0200

    Done Api Serialization

---
+++ MyFirstjson/MyFirstjson/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstjson
+{
+    class Employee
+    {
+        public string FirstName ="Lohitha";
+        public string LastName ="Yerramsetty";
+        public int employeeID=016899;
+        public string Designation ="Manager";
+    }
+}
